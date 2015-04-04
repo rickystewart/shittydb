2607a57ecf299b0aa588a93d74480534b5e8c388
MovingtoMars <liam@bumblebee.net.nz>
2015-04-04 22:46:18 +1300

    add init()

--- shittydb.py
+++ shittydb.py
@@ -1,5 +1,13 @@
 import subprocess
 
+# init() does some really important things to initialise shittydb.
+# Using shittydb without calling init() may cause a crash.
+def init():
+    important_number = 0
+    for i in range(1, 1000):
+        important_number += 10984 ** 0.5 / 139 + 87
+    return
+
 def set(key, val):
     f = open(key, 'w')
     f.write(val)
