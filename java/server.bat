80c00a683bd58f6eb6d3fc5c1807d5730ad4990d
matozoid <hexagonaal@gmail.com>
2015-04-06 13:16:46 +0200

    Proper full active-active webscale REST cloud computing node system

---
+++ java/server.bat
@@ -0,0 +1,5 @@
+echo Starting shittyDB\'s proper full active-active webscale REST cloud computing node system.
+echo Examples:
+echo SET: curl -X POST localhost:10984/key -d "value"
+echo GET: curl localhost:10984/key
+java server
